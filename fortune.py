9cc50e48a960027ccbc0cda40dabd3a93b89a182
Arjun <rao.arjun87@gmail.com>
2017-10-01 08:35:13 -0400

    Create Fortune URLs

--- fortune.py
+++ fortune.py
@@ -1,13 +1,41 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
+from datetime import date, timedelta
+from dateutil import parser
 
-class ScrapeFromFortune:
-    def __call__(self):
-        page = urlopen('http://fortune.com/2017/09/27/term-sheet-tuesday-september-26/')
-        soup = bs(page.read(), "lxml")
-        h2s = soup.find_all('h2')
-        for h2 in h2s:
-            print(h2.string)
+BASE_URL = "http://fortune.com/"
+TRAILING_SLASH = "/"
+HYPHEN = "-"
+TERM_SHEET = "term-sheet-"
+BLACKLIST_DAYS = set(["Saturday","Sunday"])
 
-s = ScrapeFromFortune()
-s()
+# class ScrapeFromFortune:
+#     def __call__(self):
+#         page = urlopen('http://fortune.com/2017/09/27/term-sheet-tuesday-september-26/')
+#         soup = bs(page.read(), "lxml")
+#         h2s = soup.find_all('h2')
+#         for h2 in h2s:
+#             print(h2.string)
+#
+# s = ScrapeFromFortune()
+# s()
+
+def generateURLs():
+    d1 = date(2011,3, 11)  # start date
+    d2 = date.today()
+    delta = d2 - d1         # timedelta
+    urls = []
+    for i in range(delta.days + 1):
+        currentDate = d1 + timedelta(days=i)
+        currentDateString = currentDate.strftime('%Y-%m-%d')
+        day = parser.parse(currentDateString).strftime("%A")
+        if day in BLACKLIST_DAYS :
+            continue
+        month = parser.parse(currentDateString).strftime("%B")
+        dateParts = currentDateString.split(HYPHEN)
+        url = BASE_URL + dateParts[0] + TRAILING_SLASH + dateParts[1] + TRAILING_SLASH + dateParts[2].lstrip("0") + TRAILING_SLASH + TERM_SHEET + day.lower() + HYPHEN + month.lower() + HYPHEN + dateParts[2].lstrip("0") + TRAILING_SLASH
+        urls.insert( i, url )
+        print (url)
+    return urls
+
+generateURLs()
--- test-scraper.py
+++ test-scraper.py
@@ -1,7 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 
-
 #sec.py
 class ScrapeSICCodesFromSEC:
     def __call__(self):
@@ -17,4 +16,44 @@ class ScrapeSICCodesFromSEC:
             print(sic_code, sic_name)
 
 s = ScrapeSICCodesFromSEC()
+s()
+
+
+import json
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
+
+class ScrapeClassificationsFromZacks:
+    def __call__(self):
+        def parse_name(name):
+            return name.strip('<span title="').split('"')[0]
+
+        page = urlopen('http://www.zacks.com/zrank/sector-industry-classification.php')
+        soup = bs(page.read())
+        main_body = soup.find('div', {'class': 'main_body'})
+        script = main_body.find('script')
+        string = str(script).split('"data"  : ')[-1]
+        string = string.rstrip('}</script>').strip().rstrip()
+        string = re.sub('{', '[{', string)
+        string = re.sub('}', '}]', string)
+
+        json_data = json.loads(string)
+
+        for group in json_data:
+            group_dict = group[0]
+
+            sector_name = parse_name(group_dict['Sector Group'])
+            sector_code = group_dict['Sector Code']
+            print('sector', sector_code, sector_name)
+
+            industry_name = parse_name(group_dict['Medium(M) Industry Group'])
+            industry_code = group_dict['Medium(M) Industry Code']
+            print('industry', industry_code, industry_name)
+
+            segment_name = parse_name(group_dict['Expanded(X) Industry Group'])
+            segment_code = group_dict['Expanded(X) Industry Code']
+            print('segment', segment_code, segment_name)
+
+s = ScrapeClassificationsFromZacks()
 s()
