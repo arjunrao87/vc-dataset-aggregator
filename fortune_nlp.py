82f838e0eccae09a5c8dcc0ae4b51a9883e5d6b8
Arjun <rao.arjun87@gmail.com>
2017-10-15 17:13:51 -0400

    Extracting vc firms and locations

--- fortune_nlp.py
+++ fortune_nlp.py
@@ -15,27 +15,53 @@ st = StanfordNERTagger('./resources/english.all.3class.distsim.crf.ser.gz','./re
 def processSentence( sentence ):
     print ( sentence )
     r.extract_keywords_from_text(sentence)
-    extract = r.get_ranked_phrases()
+    phrases = r.get_ranked_phrases()
     company = sentence.split( ",")[0]
-    categorize( company, extract )
-    entityRecognition( sentence )
+    funding_round, funding_amount = getFundingDetails( phrases )
+    vc_firms, locations = getLocationAndFirms( sentence )
+    print ( funding_round, funding_amount, vc_firms, locations )
 
-def categorize( company, phrases ):
-    company_description = None
-    lead_venture_firms = []
-    venture_firms = []
+def getFundingDetails( phrases ):
     funding_round = None
     funding_amount = []
-
     for phrase in phrases :
-        if( company in phrase or company == phrase ):
-            continue
         if( "$" in phrase or "million" in phrase ):
             funding_amount.append( phrase )
         if( "series" in phrase or "funding" in phrase ):
             funding_round = phrase
+    return funding_round, funding_amount
 
-def entityRecognition( text ):
+def getLocationAndFirms( text ):
+    text = text.replace( "-based", "")
     tokenized_text = word_tokenize(text)
     classified_text = st.tag(tokenized_text)
-    print ( classified_text )
+    vc_firms = []
+    current_vc_firm = []
+    locations = []
+    current_location = []
+    previous_classification = None
+    for phrase in classified_text :
+        if( phrase[1] == "LOCATION" ):
+            current_location.append( phrase[0])
+            previous_classification = "LOCATION"
+        if ( phrase[1] == "O" and previous_classification == "LOCATION" ):
+                previous_classification = None
+                current_location_string = " ".join( current_location )
+                locations.append(current_location_string)
+                current_location = []
+        if( phrase[1] == "ORGANIZATION" or phrase[1] == "PERSON" ):
+            current_vc_firm.append(phrase[0])
+            previous_classification = "ORGANIZATION"
+        if ( phrase[1] == "O" and previous_classification == "ORGANIZATION" ):
+            if( current_vc_firm ):
+                previous_classification = None
+                current_vc_firm_string = " ".join( current_vc_firm )
+                vc_firms.append(current_vc_firm_string)
+                current_vc_firm = []
+    if current_vc_firm and current_vc_firm not in vc_firms:
+        current_vc_firm_string = " ".join( current_vc_firm )
+        vc_firms.append( current_vc_firm_string )
+    if current_location and current_location not in locations:
+        current_location_string = " ".join( current_location )
+        locations.append( current_location_string )
+    return vc_firms, locations
