2000207ef9809af2e0e45e9e342133f88aa8fdf7
Arjun <rao.arjun87@gmail.com>
2017-10-14 23:28:29 -0400

    Adding Say Thanks

--- fortune_nlp.py
+++ fortune_nlp.py
@@ -2,6 +2,7 @@ from rake_nltk import Rake
 
 r = Rake(["the", "and", "by","in", "raised", "was", "other", "joined", "round", "-", "based","has","fortune", "read", "more", "from", "an"], [","]) # Uses stopwords for english from NLTK, and all puntuation characters.
 
+# http://textminingonline.com/getting-started-with-keyword-extraction
 # http://www.nltk.org/book/ch07.html
 # https://github.com/csurfer/rake-nltk
 def processSentence( sentence ):
